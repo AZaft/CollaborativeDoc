0a51d615283be650436760229b1d29b093715728
Anees Ziafat <ziafatanees@gmail.com>
2022-04-11 18:12:55 -0400

    Began work on multiple docs/users

--- client/index.js
+++ client/index.js
@@ -1,5 +1,28 @@
 
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [ 'link', 'image', 'formula' ],                   // Image
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],        // custom dropdown
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
 var quill = new Quill('#editor', {
+    modules: {
+        toolbar: toolbarOptions
+    },
+
     theme: 'snow'
 });
 
